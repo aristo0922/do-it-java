2be72c3e34e6d391e302dec2fe61b13697dec221
aristo <dkfud2121@naver.com>
2023-02-22 23:55:58 +0900

    error : over time

--- Ch01/src/prac/bubble/BaekJoon23970.java
+++ Ch01/src/prac/bubble/BaekJoon23970.java
@@ -1,5 +1,7 @@
 package prac.bubble;
 
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class BaekJoon23970 {
@@ -8,8 +10,12 @@ public class BaekJoon23970 {
     }
 
     private static void bubble_sort(int[] a, int[] b, int size){
-        int is_matched =0;
-        int is_printed =0;
+
+        if (Arrays.equals(a, b)) {
+            System.out.println(1);
+            return ;
+        }
+
         for(int i=1; i<size; i++){
             int flag=0;
             for(int j=0; j<size-i; j++){
@@ -17,45 +23,31 @@ public class BaekJoon23970 {
                     swap(a, j, j+1);
                     flag++;
                 }
-                is_matched = match(a, b);
-                if(is_matched == 1){
-                    System.out.println(1);
-                    is_printed=1;
+                if(a[j+1]==b[j+1]){
+                    if (Arrays.equals(a, b)) {
+                        System.out.println(1);
+                        return ;
+                    }
                 }
             }
-            if(flag ==0) break;
+            if(flag == 0) break;
         }
-        if(is_printed==0){System.out.println(0);}
+        System.out.println(0);
     }
 
-    private static int match(int[]a, int[] b){
-        for(int i=0;i<a.length; i++){
-            if(a[i] == b[i]){
-                continue;
-            }else{
-                return 0;
-            }
-        }
-        return 1;
-    }
     private static void swap(int[] a, int left, int right){
         int temp = a[left];
         a[left]=a[right];
         a[right]=temp;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
 
         int N = scan.nextInt();
         int[] list1 = new int[N];
         int[] list2 = new int[N];
 
-        if(N<5){
-            return ;
-        }else if(N>10000){
-            return ;
-        }
         for(int i=0; i<N; i++){
             list1[i]=scan.nextInt();
         }
@@ -64,8 +56,6 @@ public class BaekJoon23970 {
         }
 
         bubble_sort(list1, list2);
-
-
 
     }
 }
