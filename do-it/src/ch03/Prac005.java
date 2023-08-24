9f4af34572288d8adc9df82c78ba661c0373e78b
aristo <dkfud2121@naver.com>
2023-08-24 15:05:18 +0900

    ch03: prac005: 나머지 합 구하기

---
+++ do-it/src/ch03/Prac005.java
@@ -0,0 +1,41 @@
+package ch03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Prac005 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        long N = Integer.parseInt(stringTokenizer.nextToken());
+        long M = Integer.parseInt(stringTokenizer.nextToken());
+
+        long[] inputSum = new long[N+1];
+        stringTokenizer=new StringTokenizer(bufferedReader.readLine());
+        for(int i=1;i<=N;i++){
+            inputSum[i]=inputSum[i-1]+Integer.parseInt(stringTokenizer.nextToken());
+        }
+
+        long result =0;
+        long[] remainder = new int[M];
+        for(long i=1;i<=N;i++){
+            inputSum[i]%=M;
+            if(inputSum[i]==0){
+                result++;
+            }
+            remainder[inputSum[i]]++;
+
+        }
+
+        for(int i=0;i<M;i++){
+            if(remainder[i]>1){
+                result += (remainder[i]*(remainder[i]-1))/2;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
