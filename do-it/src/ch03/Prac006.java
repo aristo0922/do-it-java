05bfa55f5afe0ae3d8c84d4ee208a2bad9d3eac4
aristo <dkfud2121@naver.com>
2023-09-15 22:26:35 +0900

    prac006

--- do-it/src/ch03/Prac006.java
+++ do-it/src/ch03/Prac006.java
@@ -1,13 +1,20 @@
 package ch03;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 // 백준 2018
 public class Prac006 {
     public static void main(String[] args) {
-        int result;
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextBigInteger().toString();
 
+        int result =0;
+        String [] toList = input.split("");
+
+        for(int i=0; i< input.length(); i++){
+            result += Integer.parseInt(toList[i]);
+        }
+
+        System.out.println(result);
     }
 }
